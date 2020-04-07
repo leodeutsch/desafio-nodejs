e2962d0b751386662a9762db191dd7b9dda57eef
Leonardo Rocha <leorocha97@gmail.com>
2020-04-07 19:41:19 -0300

    primeiro teste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
+const { uuid } = require("uuidv4")
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -9,25 +8,77 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+// const likes = 0
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repository)
+
+  return response.json(repositories)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repositoriesIndex = repositories.find(repository => repository.id === id)
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository ID' })
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories[repositoriesIndex] = repository
+
+  return response.json(repository)
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params
+
+  const repositoriesIndex = repositories.find(repository => repository.id === id)
+
+  if (repositoriesIndex < 0) {
+    return response.status(400).json({ error: 'Invalid repository ID' })
+  }
+
+  repositories.splice(repositoriesIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repository = repositories.find(repository => repository.id === id)
+
+  if (repository < 0) {
+    return response.status(400).send()
+  }
+
+  repository.likes += 1
+
+  return response.json(repository)
 });
 
 module.exports = app;
